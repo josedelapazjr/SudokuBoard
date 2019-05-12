44803e3bbf49e2bb0ab5f74f4514108a2462a4bc
Jose de la Paz Jr <jose.delapaz.jr@gmail.com>
2019-05-12 06:12:01 -0500

    Initial commit.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Foobar from './Foobar';
+
+function App() {
+  return (
+    <div className="App">
+      Welcome to Sudoku!
+      <Foobar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Foobar.js
@@ -0,0 +1,55 @@
+import React, {Component} from 'react';
+import Button from '@material-ui/core/Button';
+import Paper from "@material-ui/core/Paper";
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
+class Foobar extends Component {
+
+  renderRow(data, index) {
+    return(
+      <TableRow key={index}>
+        {data && data.map( (number, cellIndex) => {
+          return <TableCell key={cellIndex}>{number > 0 ? number : null}</TableCell>
+        })}
+      </TableRow>
+    );
+
+  }
+
+  render() {
+    const data = '016002400320009000040103000005000069009050300630000800000306010000400072004900680';
+    const dataLength = data.length;
+    let dataArray = [];
+    for(let index = 0; index < dataLength; index++) {
+      dataArray.push(parseInt(data.charAt(index)));
+    }
+    console.log('dataArray: ', dataArray);    
+    let foobar = [];
+    for(let index = 0; index < 9; index++ ) {
+      const startIndex = index * 9;
+      const endIndex = startIndex + 9;
+      foobar.push(dataArray.slice(startIndex, endIndex));
+    }
+    console.log('foobar: ', foobar);
+    return(
+      <div>
+      <Button variant="outlined" color="primary">Testing</Button>
+        <Paper className="container">
+        <Table>
+          <TableBody>
+            {foobar && foobar.map( (row, index) => 
+              this.renderRow(row, index)
+            )}
+          </TableBody>
+        </Table>
+        </Paper>
+        </div>
+    );
+  }
+};
+
+export default Foobar;
