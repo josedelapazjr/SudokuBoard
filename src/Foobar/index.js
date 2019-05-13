60b8227249619dded6899f86277ec150e1fc817f
Jose de la Paz Jr <jose.delapaz.jr@gmail.com>
2019-05-13 16:09:45 -0500

    Add some logic (need some refactoring).

--- src/Foobar/index.js
+++ src/Foobar/index.js
@@ -11,18 +11,33 @@ import styles from './styles';
 
 class Foobar extends Component {
 
-  getUnusedNumbers = (row) => {
-    return [1,2,3,4,5,7,8,9].filter(number => !row.includes(number.toString()))
+  getUnusedNumber = (peers) => {
+    const {dataFoobar} = this.state;
+    const result =  ['1','2','3','4','5','7','8','9'].filter(number => {
+      var isValid = true;
+      for(let index = 0; index < peers.length; index++) {
+        const peer = peers[index];
+        if(dataFoobar[peer].value === number){
+          isValid = false;
+          break;
+        }
+      }
+      return isValid; 
+    });
+    return result;
   }
 
+
   renderRow(data, index) {
+    const {dataFoobar} = this.state;
     return(
       <TableRow key={index}>
-        {data && data.map( (number, cellIndex) => {
-          return <TableCell key={cellIndex}>
-            {/* <input value={number > 0 ? number : null}></input> */}
-            {/* {number != '0' ? number : '-'} */}
-            {number}
+        {data && data.map((id, index) => {
+          const unusedNumbers = this.getUnusedNumber(dataFoobar[id].peers);
+          const square = dataFoobar[id];
+          const value = square.value;
+          return <TableCell key={id}>
+            <input id={square.id} value={value} disabled={value !== '0' ? "disabled" : null}/>
           </TableCell>
         })}
       </TableRow>
@@ -30,41 +45,127 @@ class Foobar extends Component {
 
   }
 
-  render() {
+  cross = (data1, data2) => {
+    let result = [];
+      for(let rowIndex = 0; rowIndex < data1.length; rowIndex++) {
+        for(let columnIndex = 0; columnIndex < data2.length; columnIndex++) {
+            result.push(data1[rowIndex] + data2[columnIndex]);
+          }
+      }  
+      return result;
+  }
+  generateFoobar = () => {
+    const rows = "1,2,3,4,5,6,7,8,9".split(',');
+    const columns = "A,B,C,D,E,F,G,H,I".split(',');
+
+    // SQUARES
+    const SQUARES = this.cross(columns, rows);
+    // console.log('squares: ', squares);
+
+
+    // VERTICAL UNIT
+    let VERTICAL_UNITS = []
+    for (var i=0; i<rows.length; i++) {
+        VERTICAL_UNITS.push(this.cross(columns, [rows[i]])); 
+    }
+
+    // HORIZONTAL UNIT
+    let HORIZONTAL_UNITS = []
+    for (var i=0; i<columns.length; i++) {
+      HORIZONTAL_UNITS.push(this.cross([columns[i]],rows)); 
+    }
+    //console.log('unitList: ', unitList);
+
+    // SQUARE UNITS
+    let SQUARE_UNITS = []
+    var groupCols = ["A,B,C", "D,E,F", "G,H,I"];
+    var groupRows = ["1,2,3", "4,5,6", "7,8,9"];
+    for (var c=0;c<groupCols.length;c++) {
+      for (var r=0;r<groupRows.length;r++) {
+        SQUARE_UNITS.push(this.cross(groupCols[c].split(','), groupRows[r].split(',')))
+      }
+    }
+
+    const UNITLIST = [...HORIZONTAL_UNITS, ...VERTICAL_UNITS, ...SQUARE_UNITS]
+    let UNITS = {};
+    let PEERS = {};
+    for(let index = 0; index < SQUARES.length; index ++) {
+      const square = SQUARES[index];
+      const squareUnits = UNITLIST.filter(unit => unit.includes(square));
+      let squarePeers = squareUnits
+        .reduce((a,b) =>[
+          ...a.filter(data => data !==square), 
+          ...b.filter(data => data !==square)
+        ], [])
+      // Remove duplicates
+      squarePeers = squarePeers.filter((v,i) => {
+        return squarePeers.indexOf(v) === i
+      })
+      UNITS[square] = squareUnits;
+      PEERS[square] = squarePeers;
+    }
+
+    console.log('squarePeers: ', PEERS['A1']);
+    return {
+      squares: SQUARES,
+      peers: PEERS,
+    }
+  }
+
+  componentWillMount = () => {
+    console.log('rendering');
     const {classes} = this.props;
+    const foobar = this.generateFoobar();
     const data = '016002400320009000040103000005000069009050300630000800000306010000400072004900680';
     const dataLength = data.length;
     let dataArray = [];
+    let dataFoobar = {};
+    const squares = foobar['squares'];
+    const peers = foobar['peers'];
     for(let index = 0; index < dataLength; index++) {
-      // dataArray.push(parseInt(data.charAt(index)));
-      // dataArray.push(data.charAt(index));
       let numberChar = data.charAt(index);
+      const square = squares[index];
+      const squarePeers = peers[square];
       dataArray.push(numberChar);
+      let availableNumbers = [];
+      dataFoobar[square] = {
+        value: numberChar,
+        id: square,
+        peers: squarePeers
+      }
     }
-    console.log('dataArray: ', dataArray);    
-    let foobar = [];
+    console.log('dataFoobar: ', dataFoobar);
+    console.log('dataArray: ', dataArray);
+
+    let squaresArray= [];
     for(let index = 0; index < 9; index++ ) {
       const startIndex = index * 9;
       const endIndex = startIndex + 9;
-      const rowArray = dataArray.slice(startIndex, endIndex)
-      foobar.push(rowArray);
+      const rowArray = squares.slice(startIndex, endIndex)
+      squaresArray.push(rowArray);
     }
-    console.log('foobar: ', foobar);
-    foobar = foobar.map(rowArray => {
-      const unusedNumbers = this.getUnusedNumbers(rowArray);
-      let newArray = rowArray.map(data => {
-        return data === '0' ? unusedNumbers.toString() : data;
-      });
-      console.log('newArray: ', newArray);
-      return newArray;
-    })
+
+    this.setState({
+      squares: squares,
+      dataArray: dataArray,
+      dataFoobar: dataFoobar,
+      squaresArray: squaresArray,
+    }); 
+  }
+
+  render() {
+    console.log('rendering');
+    const {classes} = this.props;
+    const {squaresArray} = this.state;
+    console.log('squaresArray: ', squaresArray);
+  
     return(
       <div className={classes.root}>
         <Button variant="outlined" color="primary">Testing</Button>
         <div className={classes.tableContainer}>
           <Table>
             <TableBody>
-              {foobar && foobar.map( (row, index) => 
+              {squaresArray && squaresArray.map( (row, index) => 
                 this.renderRow(row, index)
               )}
             </TableBody>
@@ -74,5 +175,6 @@ class Foobar extends Component {
     );
   }
 };
+
 
 export default injectSheet(styles)(Foobar);
